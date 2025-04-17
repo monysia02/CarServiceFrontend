5cd4cba4baab70bc48ff96cc8cdfd8be796330fd
Kacper Kawczak <kacper@kawczak.pl>
2025-04-17 16:40:03 +0200

    vite config tests
    Signed-off-by: Kacper Kawczak <kacper@kawczak.pl>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
 });
